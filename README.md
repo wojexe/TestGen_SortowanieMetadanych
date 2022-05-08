eea9b8948f7f75c4cc9f15956efd7723ffb523b0
wojexe <21208490+wojexe@users.noreply.github.com>
2022-05-08 20:55:03 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Generator testów
+
+Do zadania **7_SortowanieMetadanych**, MP 2021/2022 UJ
+
+Testy generują się do folderu `./tests`, chyba, że zastąpimy wartość zmiennej `OUTPUT_FILE_PATH` odpowiednią ścieżką do pliku docelowego (kolejne foldery, w których plik ma się znajdować muszą istnieć).
+
+Aby wygenerować test, należy mieć zainstalowane `pnpm` (odpowiednio `npm`) i w głównym folderze sklonowanego repozytorium wpisać komendę `pnpm run gen` (odpowiednio `npm run gen`).
---
+++ testGen.js
@@ -0,0 +1,78 @@
+// Generator testow do zadania 07_SortowanieMetadanych
+
+const OUTPUT_FILE_PATH = "";
+const TEST_COUNT = 3;
+
+const maxRows = 10e5;
+const maxColumns = 15;
+const numberMagn = 10e4;
+
+const FILE_ENCODING = "ascii";
+
+import randomWords from "random-words";
+import short from "short-uuid";
+import * as fs from "fs/promises";
+import { access } from "fs";
+
+async function saveToFile(data) {
+  let path = OUTPUT_FILE_PATH;
+
+  if (path === "") {
+    let directoryExists = true;
+
+    access("./tests", (e) => {
+      if (e !== null) directoryExists = false;
+    });
+
+    if (!directoryExists) await fs.mkdir("tests");
+
+    path = "./tests/" + short().new() + ".in";
+  }
+
+  const handle = await fs
+    .open(path, "w")
+    .catch((e) => console.error(`error ${e}`));
+
+  await handle
+    .writeFile(data, { encoding: FILE_ENCODING })
+    .catch((e) => console.error(`errror ${e}`));
+}
+
+function generate(testCount) {
+  let test = "";
+
+  test += testCount + "\n";
+
+  while (testCount-- > 0) {
+    const columnCount = 3 + Math.floor(Math.random() * (maxColumns - 2));
+
+    const rowCount = 1 + Math.floor(Math.random() * (maxRows - 1));
+    const colSortBy = columnCount - Math.floor(Math.random() * columnCount);
+    const isDescending = Math.random() < 0.5;
+
+    test += `${rowCount},${colSortBy},${isDescending ? "-1" : "1"}\n`;
+
+    test += randomWords(columnCount).join(",") + "\n";
+
+    let isInteger = Array.from(
+      { length: columnCount },
+      (_) => Math.random() < 0.5
+    );
+
+    test += Array.from({ length: rowCount }, (_) =>
+      Array(...isInteger)
+        .map((isInt) =>
+          isInt ? Math.floor(Math.random() * numberMagn) : randomWords()
+        )
+        .join(",")
+    ).join("\n");
+
+    test += "\n";
+  }
+
+  return test;
+}
+
+const tests = generate(TEST_COUNT);
+
+saveToFile(tests);
