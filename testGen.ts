6e0d50048430c2ff408fb31c1579c1cb18b2a857
Wojciech Sierotowicz <wojexe@gmail.com>
2023-05-14 04:23:52 +0200

    Generate answers as well

--- testGen.ts
+++ testGen.ts
@@ -9,14 +9,25 @@ const numberMagn = 1e4;
 
 const FILE_ENCODING = "ascii";
 
-import words from "random-words"; // bad types
-import { nanoid } from "nanoid";
 import * as fs from "fs/promises";
 import path from "path";
 
-async function saveToFile(data: string) {
-  let filepath: string;
+import words from "random-words"; // has wrong types
+import { nanoid } from "nanoid";
 
+type Test = {
+  meta: number[];
+  headers: string[];
+  rows: Array<Array<string | number>>;
+  sorted: Array<Array<string | number>>;
+};
+
+type Data = {
+  input: string;
+  output: string;
+};
+
+async function saveToFile(data: Data) {
   try {
     await fs.access(OUTPUT_DIR);
   } catch (e) {
@@ -24,16 +35,26 @@ async function saveToFile(data: string) {
     await fs.mkdir(OUTPUT_DIR, { recursive: true });
   }
 
-  filepath = path.join(OUTPUT_DIR, nanoid(8) + ".in");
+  const testID = nanoid(8);
 
-  const handle = await fs.open(filepath, "w");
-  await handle.writeFile(data, { encoding: FILE_ENCODING });
+  const paths = {
+    in: path.join(OUTPUT_DIR, testID + ".in"),
+    out: path.join(OUTPUT_DIR, testID + ".out"),
+  };
+
+  const handles = {
+    in: await fs.open(paths.in, "w"),
+    out: await fs.open(paths.out, "w"),
+  };
+
+  console.log(`Generated test:\n  IN: ${paths.in}\n OUT: ${paths.out}`);
+
+  await handles.in.writeFile(data.input, { encoding: FILE_ENCODING });
+  await handles.out.writeFile(data.output, { encoding: FILE_ENCODING });
 }
 
-function generate(testCount: number) {
-  let test = "";
-
-  test += testCount + "\n";
+function generate(testCount: number): { input: string; output: string } {
+  const tests: Test[] = [];
 
   while (testCount-- > 0) {
     const columnCount = 3 + Math.floor(Math.random() * (maxColumns - 2));
@@ -42,31 +63,76 @@ function generate(testCount: number) {
     const colSortBy = columnCount - Math.floor(Math.random() * columnCount);
     const isDescending = Math.random() < 0.5;
 
-    test += `${rowCount},${colSortBy},${isDescending ? "-1" : "1"}\n`;
+    const meta = [rowCount, colSortBy, isDescending ? -1 : 1];
 
     // @ts-ignore
-    test += words(columnCount).join(",") + "\n";
+    const headers: string[] = words(columnCount);
 
-    let isInteger = Array.from(
+    const isInteger: boolean[] = Array.from(
       { length: columnCount },
       (_) => Math.random() < 0.5
     );
 
-    test += Array.from({ length: rowCount }, (_) =>
-      Array(...isInteger)
-        .map((isInt) =>
-          // @ts-ignore
-          isInt ? Math.floor(Math.random() * numberMagn) : words(1)[0]
-        )
-        .join(",")
-    ).join("\n");
+    const rows: Test["rows"] = Array.from({ length: rowCount }, (_) =>
+      isInteger.map((bool) =>
+        // @ts-ignore
+        bool ? Math.floor(Math.random() * numberMagn) : words(1)[0]
+      )
+    );
 
-    test += "\n";
+    // Generate the answer
+    let sorted: Test["sorted"] = [...rows];
+
+    const index = colSortBy - 1;
+
+    if (isInteger[index]) {
+      if (isDescending)
+        (sorted as number[][]).sort((rowA, rowB) => rowB[index] - rowA[index]);
+      else
+        (sorted as number[][]).sort((rowA, rowB) => rowA[index] - rowB[index]);
+    } else {
+      if (isDescending)
+        (sorted as string[][]).sort((rowA, rowB) =>
+          rowB[index].localeCompare(rowA[index])
+        );
+      else
+        (sorted as string[][]).sort((rowA, rowB) =>
+          rowA[index].localeCompare(rowB[index])
+        );
+    }
+
+    sorted.unshift([...headers]);
+
+    if (index !== 0) {
+      for (let row of sorted) {
+        for (let i = index - 1; i >= 0; i--) {
+          const tmp = row[i + 1];
+          row[i + 1] = row[i];
+          row[i] = tmp;
+        }
+      }
+    }
+
+    tests.push({ meta, headers, rows, sorted });
   }
 
-  return test;
+  let input = tests.length + "\n";
+  let output = "";
+
+  for (let test of tests) {
+    input += test.meta.join(",") + "\n";
+    input += test.headers.join(",") + "\n";
+    input += test.rows.map((row) => row.join(",")).join("\n");
+
+    output += test.sorted.map((row) => row.join(",")).join("\n");
+
+    input += "\n";
+    output += "\n\n";
+  }
+
+  return { input, output };
 }
 
-const tests = generate(TEST_COUNT);
+const test = generate(TEST_COUNT);
 
-saveToFile(tests);
+saveToFile(test);
